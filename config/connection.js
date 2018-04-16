33a0382d18cacf155c4c3b7abec62c0a654e0383
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-15 20:33:21 -0500

    jaws db code added

--- config/connection.js
+++ config/connection.js
@@ -6,22 +6,28 @@
 var mysql = require("mysql")
 
 //set up connection to burger_db
-var connection  = mysql.createConnection({
-    port:8889,
-    host:"localhost",
-    user:"root",
-    password: "root",
-    database: "burgers_db"
-})
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        port: 8889,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    })
+}
 
 //connect to db
 
-connection.connect(function(err){
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         throw err
         return
     }
-console.log("connected as id: " + connection.threadId)
+    console.log("connected as id: " + connection.threadId)
 })
 
 //export connection to require in orm.js
